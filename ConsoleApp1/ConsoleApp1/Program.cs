e570990d7be7d9edf1073513efd407c5d3804bba
pedrojpzzz <pedrojpzzz@gmail.com>
2019-06-18 21:48:20 +0200

    Console App example step 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key...");
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+            if (keyInfo.Key.Equals(ConsoleKey.A))
+            {
+                Console.WriteLine("You pressed A key!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
